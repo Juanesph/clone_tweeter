eecfcf4f48c0223bd0b21b6cb542a1f38c7f5f19
JuanesLinux <juanesph@gmail.com>
2020-05-16 18:43:34 -0400

    added links to navbar

---
+++ app/controllers/tweets_controller.rb
@@ -0,0 +1,36 @@
+class TweetsController < ApplicationController
+  def index
+    @tweets = Tweet.all
+  end
+
+  def new
+    @tweet = Tweet.new
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def create
+    @tweet = Tweet.new(tweet_params)
+    if @tweet.save
+      flash[:notice] = "Tu Tweet ha sido creado"      
+    else
+      flash[:notice] = "el tweet no se creo"      
+    end
+    redirect_to "/tweets"
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+  def tweet_params
+    params.require(:tweet).permit(:content, :image_url)
+  end
+end
