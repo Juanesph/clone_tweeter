29b4955fa405aa387fc9f008233b1718968b1114
JuanesLinux <juanesph@gmail.com>
2020-05-19 10:35:37 -0400

    like function workin

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -5,9 +5,10 @@ class LikesController < ApplicationController
     @like = Like.find_by(user_id: current_user.id, tweet_id: params[:tweet_id])
     if @like && @like.active
       @like.update_attribute(:active, 'false')
+      @like.destroy
     elsif @like && !@like.active
       @like.update_attribute(:active, 'true')
-      @like.destroy
+      
     else
       @like = Like.create(user_id: current_user.id, tweet_id: params[:tweet_id])
     end  
